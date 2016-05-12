07692ba8dfca1206bf3375689397df4e32f52b57
MiniLight Aurora <MiniLightAR@Gmail.com>
2016-05-12 13:57:43 -0400

    Add udp mode support.

--- src/strongtcp.c
+++ src/strongtcp.c
@@ -9,6 +9,7 @@
 #include <netinet/ip.h>
 #include <netinet/ip6.h>
 #include <netinet/tcp.h>
+#include <netinet/udp.h>
 #include <arpa/inet.h>
 
 #include <linux/netfilter.h>            /* for NF_ACCEPT */
@@ -18,11 +19,11 @@
 #include "pcap.h"
 #endif
 
-#define MTU				1500
-#define XOR_OFFSET		12
+#define MTU		1500
+#define XOR_OFFSET	12
 #define XOR_SIZE_BIT	16
-#define HOOK_IN			1
-#define HOOK_OUT		3
+#define HOOK_IN		1
+#define HOOK_OUT	3
 
 #ifdef DEBUG
 #define LOG(x, ...) printf(x, ##__VA_ARGS__)
@@ -35,13 +36,15 @@ int queue_num = 0;
 int enable_verbose = 0;
 int enable_checksum = 0;
 int enable_addzero = 0;
+int enable_udpmode = 0;
+int socket_udp = 0;
 #ifdef DEBUG
 pcap_dumpfile dumpfile = NULL;
 #endif
 
 void print_help() {
 	printf("Usage:\n"
-		"\tstrongtcp [--verbose | -v] [--checksum | -c] [--queue num | -q]\n"
+		"\tstrongtcp [--verbose | -v] [--checksum | -c] [--queue num | -q] [--udpmode | -u]\n"
 #ifdef DEBUG
 		"\t\t[--dump file | -d]\n"
 #endif
@@ -59,6 +62,8 @@ void parse_arguments(int argc, char **argv)
 			enable_verbose = 1;
 		} else if (strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--checksum") == 0) {
 			enable_checksum = 1;
+		} else if (strcmp(argv[i], "-u") == 0 || strcmp(argv[i], "--udpmode") == 0) {
+			enable_udpmode = 1;
 		} else if (strcmp(argv[i], "-q") == 0 || strcmp(argv[i], "--queue") == 0) {
 			if (i + 1 < argc) {
 				queue_num = atoi(argv[i + 1]);
@@ -115,7 +120,7 @@ static u_int16_t tcp_cksum(u_char *pkg_data)
 {
 	struct iphdr *ip4h = (struct iphdr *) pkg_data;
 	struct tcphdr *tcph = (struct tcphdr *) (pkg_data + (ip4h->ihl * 4));
-	char tcpBuf[MTU] = {0};
+	char tcpBuf[MTU];
 
 	if(ip4h->version == 6)
 	{
@@ -123,20 +128,20 @@ static u_int16_t tcp_cksum(u_char *pkg_data)
 		struct ip6PseudoHeader {
 		    struct in6_addr ip6_src;      /* source address */
 		    struct in6_addr ip6_dst;      /* destination address */
-			u_int32_t tcp_len;
+			u_int32_t len;
 			u_int8_t zero[3]; //always zero
 			u_int8_t protocol; // = 6; //for tcp
 		} psdh;
 
 		psdh.ip6_src = ip6h->ip6_src;
 		psdh.ip6_dst = ip6h->ip6_dst;
-		psdh.tcp_len = ip6h->ip6_ctlun.ip6_un1.ip6_un1_plen;
 		memset(psdh.zero, 0, 3);
-		psdh.protocol = 6;
+		psdh.protocol = ip6h->ip6_ctlun.ip6_un1.ip6_un1_nxt;
+		psdh.len = ip6h->ip6_ctlun.ip6_un1.ip6_un1_plen;
 
 		memcpy(tcpBuf, &psdh, sizeof(struct ip6PseudoHeader));
-		memcpy(tcpBuf + sizeof(struct ip6PseudoHeader), tcph, ntohs(psdh.tcp_len));
-		return ip_cksum((u_int16_t *)tcpBuf, sizeof(struct ip6PseudoHeader) + ntohs(psdh.tcp_len));
+		memcpy(tcpBuf + sizeof(struct ip6PseudoHeader), tcph, ntohs(psdh.len));
+		return ip_cksum((u_int16_t *)tcpBuf, sizeof(struct ip6PseudoHeader) + ntohs(psdh.len));
 	}
 	else
 	{
@@ -145,39 +150,31 @@ static u_int16_t tcp_cksum(u_char *pkg_data)
 			u_int32_t ip_dst;
 			u_int8_t zero;//always zero
 			u_int8_t protocol;// = 6;//for tcp
-			u_int16_t tcp_len;
+			u_int16_t len;
 		} psdh;
 
 		psdh.ip_src = ip4h->saddr;
 		psdh.ip_dst = ip4h->daddr;
 		psdh.zero = 0;
-		psdh.protocol = 6;
-		psdh.tcp_len = htons(ntohs(ip4h->tot_len) - (ip4h->ihl * 4));
+		psdh.protocol = ip4h->protocol;
+		psdh.len = htons(ntohs(ip4h->tot_len) - (ip4h->ihl * 4));
 		memcpy(tcpBuf, &psdh, sizeof(struct ip4PseudoHeader));
-		memcpy(tcpBuf + sizeof(struct ip4PseudoHeader), tcph, ntohs(psdh.tcp_len));
-		return ip_cksum((u_int16_t *)tcpBuf, sizeof(struct ip4PseudoHeader) + ntohs(psdh.tcp_len));
+		memcpy(tcpBuf + sizeof(struct ip4PseudoHeader), tcph, ntohs(psdh.len));
+		return ip_cksum((u_int16_t *)tcpBuf, sizeof(struct ip4PseudoHeader) + ntohs(psdh.len));
 	}
 }
 
 static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nfq_data *nfa, void *data)
 {
-
-	struct nfqnl_msg_packet_hdr *ph;
-	uint32_t id = 0;
-	uint32_t pkg_data_len;
-
-	u_char *pkg_data;
-
 	LOG("entering callback\n");
 
-	ph = nfq_get_msg_packet_hdr(nfa);
-	if (ph)
-	{
-		id = ntohl(ph->packet_id);
-		LOG("HWP:0x%04x HOK:%u ID:%u ", ntohs(ph->hw_protocol), ph->hook, id);
-	}
+	struct nfqnl_msg_packet_hdr *ph = nfq_get_msg_packet_hdr(nfa);
+	uint32_t id = ntohl(ph->packet_id);
 
-	pkg_data_len = nfq_get_payload(nfa, &pkg_data);
+	LOG("HWP:0x%04x HOK:%u ID:%u ", ntohs(ph->hw_protocol), ph->hook, id);
+
+	u_char *pkg_data;
+	uint32_t pkg_data_len = nfq_get_payload(nfa, &pkg_data);
 	// nfq_get_payload() return -1 on error, otherwise > 0.
 	if (pkg_data_len > 0)
 	{
@@ -199,32 +196,87 @@ static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nfq_data *
 		}
 #endif
 
-		struct tcphdr *tcph;
+		struct tcphdr *tcph = 0;
+		struct udphdr *udph = 0;
 		struct iphdr *ip4h = (struct iphdr *) pkg_data;
+		struct ip6_hdr *ip6h = (struct ip6_hdr *) pkg_data;
+		LOG("VER:IPV%d\n", ip4h->version);
 		if(ip4h->version == 6)
 		{
-			tcph = (struct tcphdr *) (pkg_data + sizeof(struct ip6_hdr));
-			LOG("VER:IP6\n");
+			if(ip6h->ip6_ctlun.ip6_un1.ip6_un1_nxt == SOL_UDP)
+				udph = (struct udphdr *) (pkg_data + sizeof(struct ip6_hdr));
+			else if(ip6h->ip6_ctlun.ip6_un1.ip6_un1_nxt == SOL_TCP)
+				tcph = (struct tcphdr *) (pkg_data + sizeof(struct ip6_hdr));
+			else
+				return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL );
+		}
+		else if(ip4h->version == IPVERSION)
+		{
+			if(ip4h->protocol == SOL_UDP)
+				udph = (struct udphdr *) (pkg_data + (ip4h->ihl * 4));
+			else if(ip4h->protocol == SOL_TCP)
+				tcph = (struct tcphdr *) (pkg_data + (ip4h->ihl * 4));
+			else
+				return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL );
+		}
+		else
+			return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL );
+
+		if( udph )
+		{
+			if(ip4h->version == 6)
+			{
+				ip6h->ip6_ctlun.ip6_un1.ip6_un1_nxt = SOL_TCP;
+				ip6h->ip6_ctlun.ip6_un1.ip6_un1_plen = htons(ntohs(ip6h->ip6_ctlun.ip6_un1.ip6_un1_plen) - 8);
+			}
+			else
+			{
+				ip4h->protocol = SOL_TCP;
+				ip4h->tot_len = htons(ntohs(ip4h->tot_len) - 8);
+				ip4h->check = 0;
+				ip4h->check = ip_cksum((u_int16_t*)ip4h, ip4h->ihl * 4);
+			}
+			memcpy(udph, ((u_int8_t*)udph) + 8, 2);
+			memcpy(((u_int8_t*)udph) + 4, ((u_int8_t*)udph) + 10, ntohs(udph->len) - 10);
+			if (enable_checksum) {
+				tcph = (struct tcphdr *)udph;
+				tcph->check = 0;
+				tcph->check = tcp_cksum(pkg_data);
+			}
+		}
+		else if( enable_udpmode && ph->hook==HOOK_OUT && !tcph->syn && !tcph->fin )
+		{
+			if(ip4h->version == 6)
+			{
+				//To-do
+			}
+			else
+			{
+				static struct sockaddr_in packet_addr;
+				bzero(&packet_addr, sizeof(packet_addr));
+				packet_addr.sin_family = AF_INET;
+				packet_addr.sin_addr.s_addr = ip4h->daddr;
+				packet_addr.sin_port = tcph->dest;
+
+				u_int16_t tcpsize = ntohs(ip4h->tot_len) - (ip4h->ihl * 4);
+				memcpy(((u_int8_t*)tcph) + 2, ((u_int8_t*)tcph) + 4, tcpsize - 2);
+				sendto(socket_udp, tcph, tcpsize - 2, 0, (struct sockaddr *) &packet_addr, sizeof(packet_addr));
+				return nfq_set_verdict(qh, id, NF_DROP, 0, NULL );
+			}
 		}
 		else
 		{
-			tcph = (struct tcphdr *) (pkg_data + (ip4h->ihl * 4));
-			LOG("VER:IP4\n");
+			u_int32_t xor = (*(u_int32_t*) ((u_char*)tcph + XOR_OFFSET));
+			LOG("FLG XOR:0x%08x\n", ntohl(xor));
+			LOG("BEF SEQ:0x%08x ACK:0x%08x SUM:0x%04x\n", ntohl(tcph->seq), ntohl(tcph->ack_seq), ntohs(tcph->check));
+			tcph->seq ^= xor;
+			tcph->ack_seq ^= xor;
+			if (enable_checksum) {
+				tcph->check = 0;
+				tcph->check = tcp_cksum(pkg_data);
+			}
+			LOG("AFT SEQ:0x%08x ACK:0x%08x SUM:0x%04x\n", ntohl(tcph->seq), ntohl(tcph->ack_seq), ntohs(tcph->check));
 		}
-
-		u_int32_t xor = (*(u_int32_t*) ((char*)tcph + XOR_OFFSET));
-
-		LOG("FLG XOR:0x%08x\n", ntohl(xor));
-		LOG("BEF SEQ:0x%08x ACK:0x%08x SUM:0x%04x\n", ntohl(tcph->seq), ntohl(tcph->ack_seq), ntohs(tcph->check));
-		
-		tcph->seq ^= xor;
-		tcph->ack_seq ^= xor;
-		if (enable_checksum) {
-			tcph->check = 0;
-			tcph->check = tcp_cksum(pkg_data);
-		}
-
-		LOG("AFT SEQ:0x%08x ACK:0x%08x SUM:0x%04x\n", ntohl(tcph->seq), ntohl(tcph->ack_seq), ntohs(tcph->check));
 
 #ifdef DEBUG
 		if(dumpfile && ph->hook == HOOK_IN)
@@ -246,14 +298,13 @@ static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nfq_data *
 	}
 
 	ERROR("nfq_get_payload() not return > 0\n");
-	return nfq_set_verdict(qh, id, NF_DROP, 0, NULL );
+	return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL );
 }
 
 int main(int argc, char **argv)
 {
 	struct nfq_handle *h;
 	struct nfq_q_handle *qh;
-	int fd;
 	int rv;
 	char buf[4096] __attribute__ ((aligned));
 	
@@ -266,6 +317,17 @@ int main(int argc, char **argv)
 		u_int16_t test[] = {0x1234};
 		enable_addzero = ((u_int32_t)((u_int16_t)(*(u_int8_t *)test)<<8)) == 0x00001200;        // need add 0
 		LOG("addzero checksum enable: %d\n", enable_addzero);
+	}
+
+	if(enable_udpmode)
+	{
+		LOG("udp mode enable, opening UDP socket\n");
+		socket_udp = socket(AF_INET, SOCK_DGRAM, 0);
+		if (socket_udp == -1)
+		{
+			ERROR("error during socket()\n");
+			exit(1);
+		}
 	}
 
 	LOG("opening library handle\n");
@@ -305,7 +367,7 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	fd = nfq_fd(h);
+	int fd = nfq_fd(h);
 	while ((rv = recv(fd, buf, sizeof(buf), 0)) && rv >= 0)
 	{
 		LOG("pkt received\n");
